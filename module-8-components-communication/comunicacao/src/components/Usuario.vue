f53b73d8b507b7663735194380a4118738d74393
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2020-10-29 21:04:45 -0300

    Component communication

---
+++ module-8-components-communication/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+<!--            <app-usuario-info />-->
+            <bla-bla :userName="nome" />
+<!--            <bla-bla />-->
+<!--            <app-usuario-info :userName="1" />-->
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import BlaBla from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { BlaBla, AppUsuarioEditar },
+    data () {
+      return {
+       nome: 'Loro',
+      }
+    },
+    methods: {
+      alterarNome() {
+        this.nome = 'Ana';
+      },
+    }
+}
+</script>
+
+<style scoped>
+  button {
+    background-color: #42b983;
+    border: none;
+    border-radius: 0;
+    color: white;
+  }
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ module-8-components-communication/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuario: <strong>{{ inverterNome() }}</strong></p>
+<!--        <p>Nome do usuario: <strong>{{ nomeAlternativo }}</strong></p>-->
+    </div>
+</template>
+
+<script>
+export default {
+  // props: ['userName'],
+  // props: {
+  //   userName: [String/*, Array*/]
+  // },
+  props: {
+    userName: {
+      type: String,
+      // required: true,
+      default: 'Padrão',
+      // default: function () {
+      //   return Array(10).fill(0).join(',');
+      // },
+    }
+  },
+  data() {
+    return {
+      // userName: '' // conflito de nome
+      nomeAlternativo: this.userName,
+    };
+  },
+  methods: {
+    inverterNome() {
+      return this.userName.split('').reverse().join('');
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
