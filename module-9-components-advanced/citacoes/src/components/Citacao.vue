7e1664f208b04a70e91f739e521b2a1664657f7e
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2020-10-30 20:34:55 -0300

    Component slots

---
+++ module-9-components-advanced/citacoes/src/components/Citacao.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+        <slot name="autor"></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['citacao'],
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h1 {
+      color: red !important;
+    }
+</style>
---
+++ module-9-components-advanced/citacoes/src/components/Citacoes.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <Citacao>
+          <h1 slot="texto">{{ citacoes[indice].texto }}</h1>
+          <p>{{ citacoes[indice].fonte }}</p>
+          <h6 slot="autor">{{ citacoes[indice].autor }}</h6>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao';
+
+export default {
+    components: {Citacao},
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h1 {
+      color: #42b983 !important;
+    }
+</style>
