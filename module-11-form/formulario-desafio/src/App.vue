658e45f575612dc1e0a9368ec2b1cccf6cf377f3
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2020-11-07 15:08:49 -0300

    Task Form

---
+++ module-11-form/formulario-desafio/src/App.vue
@@ -0,0 +1,136 @@
+<template>
+	<div id="app">
+		<h1>Formulário Desafio</h1>
+		<div class="conteudo">
+			<form class="painel" v-if="!sent">
+				<div class="cabecalho">Formulário</div>
+				<!-- Exercicio 01 -->
+				<!-- Criar uma formulário de registro -->
+				<!-- Nome completo (Nome e Sobrenome) -->
+				<!-- Email -->
+				<!-- Senha -->
+				<!-- Armazenar Dados? (Sim/Não) -->
+
+				<!-- Exercicio 02 -->
+				<!-- Só mostrar o fomulário de não tiver sido submetido -->
+				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+				
+				<!-- Exercicio 03 -->
+				<!-- Crie um componente personalizado NomeCompleto -->
+				<!-- Esse componente deve receber Nome e Sobrenome -->
+
+				<NomeCompleto v-model="user" />
+				<rotulo nome="Email">
+					<input type="email" v-model="user.email" />
+				</rotulo>
+				<rotulo nome="Senha">
+					<input type="password" v-model="user.password" />
+				</rotulo>
+				<rotulo nome="Senha">
+					<span class="mr-4"><input type="radio" value="yes" v-model="store"> Sim</span>
+					<span class="mr-4"><input type="radio" value="no" v-model="store"> Não</span>
+				</rotulo>
+				<button @click.prevent="submit">Enviar</button>
+			</form>
+			<div class="painel" v-else>
+				<div class="cabecalho">Resultado</div>
+				<rotulo nome="Nome completo">
+					{{ user.name }}
+				</rotulo>
+				<rotulo nome="Nome completo">
+					{{ user.surname }}
+				</rotulo>
+				<rotulo nome="Email">
+					{{ user.email }}
+				</rotulo>
+				<rotulo nome="Password">
+					{{ user.password }}
+				</rotulo>
+				<rotulo nome="Armazenar">
+					{{ store }}
+				</rotulo>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from './components/NomeCompleto.vue'
+
+export default {
+	name: 'app',
+	components: { Rotulo, NomeCompleto },
+	data() {
+		return {
+			user: {},
+			store: 'no',
+			sent: false,
+		}
+	},
+	methods: {
+		submit() {
+			this.sent = true;
+		}
+	}
+}
+</script>
+
+<style>
+
+body {
+	background-color: #ECECEC;
+}
+
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	display: flex;
+	flex-direction: column;
+}
+
+.conteudo {
+	display: flex;
+}
+
+.painel {
+	flex: 1;
+	background: #FFF;
+	margin: 0px 10px;
+	padding: 20px;
+	border: 1px solid #AAA;
+	border-radius: 5px;
+}
+
+.painel .cabecalho {
+	width: 100%;
+	background-color: #DDD;
+	padding: 10px 0px;
+	border-radius: 5px;
+	font-size: 1.4rem;
+}
+
+#app form button {
+	float: right;
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+
+#app h1 {
+	font-weight: 200;
+	margin: 20px;
+	padding: 0;
+}
+
+.mr-4 {
+	margin-right: 40px;
+}
+</style>
---
+++ module-11-form/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+			<rotulo nome="Nome">
+				<input type="text" :value="value.name" @input="updateName" />
+			</rotulo>
+			<rotulo nome="Sobrenome">
+				<input type="text" :value="value.surname" @input="updateSurname" />
+			</rotulo>
+    </div>
+</template>
+
+<script>
+import Rotulo from '../components/Rotulo.vue'
+
+export default {
+		components: { Rotulo },
+    props: {
+			value: {
+				type: Object,
+				required: true,
+			}
+    },
+	methods: {
+		updateName(event) {
+			this.$emit('input', {
+				name: event.target.value,
+				surname: this.value.surname
+			})
+		},
+		updateSurname(event) {
+			this.$emit('input', {
+				name: this.value.name,
+				surname: event.target.value
+			})
+		}
+	},
+}
+</script>
+
+<style scoped>
+    .rotulo {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: 15px 0px;
+        font-size: 1.4rem;
+    }
+
+    .elemento {
+        display: flex;
+        align-self: stretch;
+        margin: 10px 0px;
+    }
+
+    input, textarea, select {
+        flex: 1;
+        font-size: 1.4rem;
+        outline: none;
+        border: 1px solid #DDD;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
+
+
+    input:focus {
+        border: 1px solid #BBB;
+    }
+</style>
