52da487a78a3ab6eecf3cbb41633765dadcc2ab1
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2020-11-08 19:48:01 -0300

    Directives

--- module-12-directives/diretivas-exercicios/src/App.vue
+++ module-12-directives/diretivas-exercicios/src/App.vue
@@ -1,12 +1,54 @@
 <template>
 	<div id="app">
 		<h1>Diretivas</h1>
+		<h3 v-highlight>Usando diretivas</h3>
+		<h3 v-highlight="'blue'">Usando diretivas</h3>
+		<h3 v-highlight="color">Usando diretivas</h3>
+		<h3 v-highlight:back="color">Usando diretivas</h3>
+		<h3 v-highlight:back.timeout="color">Usando diretivas</h3>
+		<h3 v-highlight-local:back.timeout.rotate="color">Usando diretivas</h3>
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		'highlight-local': {
+			bind(el, binding) {
+				const applyColor = color => {
+					if (binding.arg === 'back') {
+						el.style.backgroundColor = color;
+					} else {
+						el.style.color = color;
+					}
+				};
+
+				let timeout = 0;
+				if (binding.modifiers['timeout']) timeout = 3000;
+
+				const color1 = binding.value;
+				const color2 = 'purple';
+				let currentColor = color1;
+
+				setTimeout(() => {
+					if (binding.modifiers['rotate']) {
+						setInterval(() => {
+							currentColor = currentColor === color1 ? color2 : color1;
+							applyColor(currentColor);
+						}, 500);
+					} {
+						applyColor(binding.value);
+					}
+
+				}, timeout);
+			}
+		}
+	},
+	data() {
+		return {
+			color: 'yellow',
+		}
+	}
 }
 </script>
 
--- module-12-directives/diretivas-exercicios/src/main.js
+++ module-12-directives/diretivas-exercicios/src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.directive('highlight', {
+	bind(el, binding) {
 
+		let atraso = 0;
+		if (binding.modifiers['atrasar']) atraso = 3000;
+
+		setTimeout(() => {
+			if (binding.arg === 'fundo') {
+				el.style.backgroundColor = binding.value;
+			} else {
+				el.style.color = binding.value;
+			}
+		}, atraso);
+	}
+});
 new Vue({
 	render: h => h(App),
 }).$mount('#app')
