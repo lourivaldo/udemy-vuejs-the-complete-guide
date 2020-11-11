c18322177fa0ef18d2e930ed6972439a528d7679
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-11 17:19:24 -0300

    Router params by props

--- module-16-router/rotas-exercicios/src/components/user/User.vue
+++ module-16-router/rotas-exercicios/src/components/user/User.vue
@@ -9,16 +9,17 @@
 
 <script>
 export default {
-    data() {
-        return {
-            id: this.$route.params.id,
-        }
-    },
-    watch: {
-      $route(to, from) {
-          this.id = to.params.id;
-      }
-    },
+    props: ['id'],
+    // data() {
+    //     return {
+    //         id: this.$route.params.id,
+    //     }
+    // },
+    // watch: {
+    //   $route(to, from) {
+    //       this.id = to.params.id;
+    //   }
+    // },
     methods: {
         goHome() {
             // this.$router.push('/')
--- module-16-router/rotas-exercicios/src/router.js
+++ module-16-router/rotas-exercicios/src/router.js
@@ -11,6 +11,6 @@ export default new Router({
     mode: 'history',
     routes:[
         {path: '/', component: Home},
-        {path: '/user/:id', component: User},
+        {path: '/user/:id', component: User, props: true},
     ]
 })
