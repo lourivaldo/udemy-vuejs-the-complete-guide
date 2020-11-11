d3594b30d3aa66a334b20e66275e3b551583afd3
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-11 17:16:43 -0300

    Router params

--- module-16-router/rotas-exercicios/src/components/user/User.vue
+++ module-16-router/rotas-exercicios/src/components/user/User.vue
@@ -1,12 +1,30 @@
 <template>
 <div class="usuario">
     <h2>Usuários</h2>
+    <hr>
+    <p><strong>ID</strong> {{ id }}</p>
+    <button sucesso @click="goHome">Ir para home</button>
 </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            id: this.$route.params.id,
+        }
+    },
+    watch: {
+      $route(to, from) {
+          this.id = to.params.id;
+      }
+    },
+    methods: {
+        goHome() {
+            // this.$router.push('/')
+            this.$router.push({path: '/'})
+        }
+    }
 }
 </script>
 
--- module-16-router/rotas-exercicios/src/router.js
+++ module-16-router/rotas-exercicios/src/router.js
@@ -11,6 +11,6 @@ export default new Router({
     mode: 'history',
     routes:[
         {path: '/', component: Home},
-        {path: '/user', component: User},
+        {path: '/user/:id', component: User},
     ]
 })
