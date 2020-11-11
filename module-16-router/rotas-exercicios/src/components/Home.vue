47e8db677018d8cf0bf6f526863d9f76041e8902
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-11 16:46:45 -0300

    Router config

---
+++ module-16-router/rotas-exercicios/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="inicio">
+    <h2>Inicio</h2>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ module-16-router/rotas-exercicios/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from './components/Home'
+import User from './components/user/User'
+
+Vue.use(Router);
+
+export default new Router({
+    routes:[
+        {path: '/', component: Home},
+        {path: '/user', component: User},
+    ]
+})
