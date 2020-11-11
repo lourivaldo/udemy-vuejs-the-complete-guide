f478c04815d94fa34409d05b11ceb3b2b8af1800
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-11 17:06:08 -0300

    Router active

---
+++ module-16-router/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <router-link to="/" tag="li" active-class="active" exact>
+                <a>Início</a>
+            </router-link>
+            <router-link to="/user" tag="li" active-class="active">
+                <a>Usuário</a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
