0e385c052c66b1d703874d5d489e17b7f908a33a
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-15 13:23:18 -0300

    Vuex mapGetters

--- module-17-vuex/vuex-exercicios/src/components/Resumo.vue
+++ module-17-vuex/vuex-exercicios/src/components/Resumo.vue
@@ -9,15 +9,21 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    computed: {
-        total() {
-            return this.$store.getters.valorTotal
-        },
-        produtos() {
-            return this.$store.state.produtos;
-        }
-    },
+    // computed: {
+    //     total() {
+    //         return this.$store.getters.valorTotal
+    //     },
+    //     produtos() {
+    //         return this.$store.state.produtos;
+    //     }
+    // },
+    // computed: mapGetters(['valorTotal']),
+    computed: mapGetters({
+        total: 'valorTotal'
+    }),
 }
 </script>
 
