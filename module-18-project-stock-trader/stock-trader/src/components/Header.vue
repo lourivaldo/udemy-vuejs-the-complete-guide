043cbffb07d95e2ceb112980172347ee733853a6
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-17 15:07:59 -0300

    Stock Trader Menu

--- module-18-project-stock-trader/stock-trader/src/components/Header.vue
+++ module-18-project-stock-trader/stock-trader/src/components/Header.vue
@@ -1,5 +1,44 @@
 <template>
-    <h1>Header</h1>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase mr-4">
+            <span>Stock</span>
+            <span class="font-weight-light">Trader</span>
+        </v-toolbar-title>
+
+        <v-toolbar-items>
+            <v-btn flat to="/">Home</v-btn>
+            <v-btn flat to="/portfolio">Portfolio</v-btn>
+            <v-btn flat to="/stocks">Stocks</v-btn>
+        </v-toolbar-items>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn flat>Finish Day</v-btn>
+            <v-menu offset-y>
+                <v-btn flat slot="activator">Save & Load</v-btn>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title>Salve Data</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-title>Load Data</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+<!--        <v-btn icon>-->
+<!--            <v-icon>mdi-magnify</v-icon>-->
+<!--        </v-btn>-->
+
+<!--        <v-btn icon>-->
+<!--            <v-icon>mdi-heart</v-icon>-->
+<!--        </v-btn>-->
+
+<!--        <v-btn icon>-->
+<!--            <v-icon>mdi-dots-vertical</v-icon>-->
+<!--        </v-btn>-->
+    </v-toolbar>
 </template>
 
 <script>
