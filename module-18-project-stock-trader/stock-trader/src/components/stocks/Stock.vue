07ed4536580f97aa8dd2e4d2fc8c7fd5645f2b65
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-17 16:45:30 -0300

    Stock Trader stocks list

--- module-18-project-stock-trader/stock-trader/src/components/stocks/Stock.vue
+++ module-18-project-stock-trader/stock-trader/src/components/stocks/Stock.vue
@@ -1,10 +1,43 @@
 <template>
-    <h1>Action Stock (buy)</h1>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>{{ stock.name }} <small>(Price: {{ stock.price }})</small></strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Amount" type="number" v-model.number="quantity"/>
+                <v-btn @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)" class="green darken-3 white--text">Buy</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
     export default {
+        props: {
+            stock: {
+                required: true,
+            }
+        },
+        data() {
+            return {
+                quantity: 0,
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity,
+                };
+                this.quantity = 0;
 
+                console.log(order)
+            }
+        }
     }
 </script>
 
