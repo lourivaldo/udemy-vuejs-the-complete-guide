8158b8248ddd2778f2eb2ce0b96c2c3be27fa046
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-20 19:52:55 -0300

    Calculator app

---
+++ module-20-bonus-4-projeto-calculadora/calculator/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+    body {
+        margin: 0;
+    }
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: #fff;
+        background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(34,34,148,0.9720247005988024) 63%, rgba(0,212,255,1) 100%);
+    }
+</style>
