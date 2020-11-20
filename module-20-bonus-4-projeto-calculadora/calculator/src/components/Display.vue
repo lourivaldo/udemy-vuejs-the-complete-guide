df8109d77962199ef168de5917a5a6ffcf1c07a6
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-20 20:14:31 -0300

    Calculator display

--- module-20-bonus-4-projeto-calculadora/calculator/src/components/Display.vue
+++ module-20-bonus-4-projeto-calculadora/calculator/src/components/Display.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="display">0</div>
+    <div class="display">{{ value }}</div>
 </template>
 
 <script>
 export default {
-
+    props: ['value']
 }
 </script>
 
 <style>
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
+
+        display: flex;
+        justify-content: flex-end;
+        padding: 20px;
+        font-size: 2.2em;
+        overflow: hidden;
+    }
 </style>
