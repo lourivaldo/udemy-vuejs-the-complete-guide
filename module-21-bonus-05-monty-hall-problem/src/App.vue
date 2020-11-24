de0c13e7e96eb54b29aa151902f1355238fde235
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-23 22:31:51 -0300

    Monty Hall #1

---
+++ module-21-bonus-05-monty-hall-problem/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+      <h1>Problema de Monty Hall</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+
+body {
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(34,34,148,0.9720247005988024) 63%, rgba(0,212,255,1) 100%);
+}
+#app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+</style>
