e55c4250d63bf67d902da13cd0cbce08bd750a99
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-24 23:04:02 -0300

    Monthy Hall #2

--- module-21-bonus-05-monty-hall-problem/src/App.vue
+++ module-21-bonus-05-monty-hall-problem/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
+  <div id='app'>
       <h1>Problema de Monty Hall</h1>
+      <Gift />
   </div>
 </template>
 
 <script>
+import Gift from './components/Gift';
 
 export default {
   name: 'App',
-  components: {
-  }
+  components: { Gift },
 }
 </script>
 
