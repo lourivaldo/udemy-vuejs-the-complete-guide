ea35d7a8a99505b349950a173f596c92ccd99982
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-11-28 16:17:49 -0300

    Backend consign+middlewares

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/index.js
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/index.js
@@ -1,6 +1,11 @@
-import express from 'express';
+const express = require('express');
+const consign = require('consign');
 
 const app = express();
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
 app.listen(3000, () => {
     console.log('Listen on localhost:3000');
