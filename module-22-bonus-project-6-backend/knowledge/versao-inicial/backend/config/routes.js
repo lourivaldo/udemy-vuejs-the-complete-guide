86a976ac576ea5f6d1f32491f3e75c5621c4d446
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-01 23:34:02 -0300

    Backend passport jwt

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/config/routes.js
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/config/routes.js
@@ -4,34 +4,42 @@ module.exports = app => {
    app.post('/validateToken', app.api.auth.validateToken);
 
    app.route('/users')
+       .all(app.config.passport.authenticate())
        .post(app.api.user.save)
        .get(app.api.user.all);
 
    app.route('/users/:id')
+       .all(app.config.passport.authenticate())
        .put(app.api.user.save)
        .get(app.api.user.get);
 
    app.route('/categories')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.get)
         .post(app.api.category.save);
 
    app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree);
 
    app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove);
 
    app.route('/articles')
+       .all(app.config.passport.authenticate())
        .post(app.api.article.save)
        .get(app.api.article.get);
 
    app.route('/articles/:id')
+       .all(app.config.passport.authenticate())
        .put(app.api.article.save)
        .get(app.api.article.getById)
        .delete(app.api.article.remove);
 
-    app.route('/categories/:id/articles')
-        .delete(app.api.article.getByCategory);
+   app.route('/categories/:id/articles')
+       .all(app.config.passport.authenticate())
+       .delete(app.api.article.getByCategory);
 };
--- module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/index.js
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.db = db
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validation.js')
     .then('./api')
