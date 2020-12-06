f1921e1afef279ce267623f83fad9bd67d677919
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-06 19:34:25 -0300

    Frontend template grid

---
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Content.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+    export default {
+        name: 'Content'
+
+    }
+</script>
+
+<style scoped>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
---
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>Cod3r</strong> 2015-2018</span>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
+
+<style scoped>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+
+</style>
