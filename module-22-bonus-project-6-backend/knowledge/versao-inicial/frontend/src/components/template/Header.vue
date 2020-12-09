75b5a6b8920dc70be0093a97319caf08379b66d6
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-08 22:37:36 -0300

    Frontend router

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Header.vue
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Header.vue
@@ -3,7 +3,9 @@
         <a class="toggle" @click.prevent="toggleMenu" v-if="!hideToggle">
             <i class="fa fa-lg" :class="icon"></i>
         </a>
-        <h1 class="title">{{ title }}</h1>
+        <h1 class="title">
+            <router-link to="/">{{ title }}</router-link>
+        </h1>
         <UserDropdown v-if="!hideUserDropdown" />
     </header>
 </template>
@@ -50,7 +52,7 @@ export default {
         text-align: center;
     }
 
-    .title a {
+    .title a, .title:hover {
         color: #fff;
         text-decoration: none;
     }
