1aef85f01e0ef74d6abe587401bd80fa2e4fe5c4
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-09 23:08:19 -0300

    Frontend home stats

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/main.js
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/main.js
@@ -8,6 +8,8 @@ import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false;
+// TEMP
+require('axios').defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6Ik5vIGFkbSIsImVtYWlsIjoibm9hZG1AZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYwNzU2NTI3OSwiZXhwIjoxNjA3ODI0NDc5fQ.G8sJn25zwKDB5wMPJQabxyszB1GQlp9phx1f0QbxxUM'
 
 new Vue({
   store,
