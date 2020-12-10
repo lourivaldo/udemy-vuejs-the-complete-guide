c40423c68206689526433122d301bb78e3b36b73
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-09 23:23:35 -0300

    Frontend admin users table

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="user-admin">
-        <h1>User Componente</h1>
+        <b-table hover striped :items="users" :fields="fields"></b-table>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
+
 export default {
     name: 'UserAdmin',
+    data() {
+        return {
+            mode: 'save',
+            user: {},
+            users: [],
+            fields: [
+                { key: 'id', label: 'Código', sortable: true },
+                { key: 'name', label: 'Nome', sortable: true },
+                { key: 'email', label: 'E-mail', sortable: true },
+                {
+                    key: 'admin',
+                    label: 'Administrador',
+                    sortable: true,
+                    formatter: value => value ? 'Sim' : 'Não',
+                },
+                { key: 'actions', label: 'Ações' },
+            ]
+        }
+    },
+    methods: {
+        loadUsers() {
+            axios.get(`${baseApiUrl}/users`).then(res => {
+                this.users = res.data;
+            })
+        },
+    },
+    mounted() {
+        this.loadUsers();
+    }
 }
 </script>
 
--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/main.js
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/main.js
@@ -9,7 +9,7 @@ import router from './config/router'
 
 Vue.config.productionTip = false;
 // TEMP
-require('axios').defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6Ik5vIGFkbSIsImVtYWlsIjoibm9hZG1AZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYwNzU2NTI3OSwiZXhwIjoxNjA3ODI0NDc5fQ.G8sJn25zwKDB5wMPJQabxyszB1GQlp9phx1f0QbxxUM'
+require('axios').defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxvcm8gViIsImVtYWlsIjoibG9yb0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjA3NTY2NzIwLCJleHAiOjE2MDc4MjU5MjB9.qyaznhU7TEoi8474D9oT5JWvDeNUvqE8V8AGOEptVMQ'
 
 new Vue({
   store,
