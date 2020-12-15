1a9d52159fe6c42dd56c69d8e11473fb449539b1
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-12-15 19:51:02 -0300

    Frontend auth logout

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="user-dropdown-content">
             <router-link to="/admin"><i class="fa fa-cogs"></i> Administração</router-link>
-            <a href=""><i class="fa fa-sign-out"></i> Sair</a>
+            <a href @click.prevent="logout"><i class="fa fa-sign-out"></i> Sair</a>
         </div>
     </div>
 </template>
@@ -17,6 +17,7 @@
 <script>
 import { mapState } from 'vuex'
 import Gravatar from 'vue-gravatar'
+import { userKey } from '@/global'
 
 export default {
     name: 'UserDropdown',
@@ -24,6 +25,13 @@ export default {
     computed: {
         ...mapState(['user'])
     },
+    methods: {
+        logout() {
+            localStorage.removeItem(userKey);
+            this.$store.commit('setUser', null);
+            this.$router.push({ name: 'auth' });
+        },
+    }
 }
 </script>
 
