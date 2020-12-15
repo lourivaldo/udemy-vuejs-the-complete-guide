f477b0bc32c14dcad470a9a10399041b36f9b479
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2020-12-14 23:17:30 -0300

    Frontend menu #1

--- module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Menu.vue
+++ module-22-bonus-project-6-backend/knowledge/versao-inicial/frontend/src/components/template/Menu.vue
@@ -1,25 +1,95 @@
 <template>
-    <aside class="menu" v-show="isMenuVisible"></aside>
+	<aside class="menu" v-show="isMenuVisible">
+		<div class="menu-filter">
+			<i class="fa fa-search fa-lg"></i>
+			<input type="text" placeholder="Digite para filtrar..." v-model="treeFilter" class="filter-field">
+		</div>
+		<Tree :data="treeData" :options="treeOptions" ref="tree" :filter="treeFilter" />
+	</aside>
 </template>
 
 <script>
-    import { mapState } from 'vuex'
+import { mapState } from 'vuex'
+import Tree from 'liquor-tree'
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
 
-    export default {
-        name: 'Menu',
-        computed: {
-            ...mapState(['isMenuVisible']),
-        }
-    }
+export default {
+  name: 'Menu',
+  components: { Tree },
+	data() {
+		return {
+			treeFilter: '',
+			treeData: this.getTreeData(),
+			treeOptions: {
+				propertyNames: { 'text': 'name' },
+				filter: {	emptyText: 'Categoria não encontrada'	},
+			},
+		}
+	},
+  computed: {
+      ...mapState(['isMenuVisible']),
+  },
+	methods: {
+    getTreeData() {
+      const url = `${ baseApiUrl }/categories/tree`;
+      return axios.get(url).then(res => res.data);
+    },
+	}
+}
 </script>
 
 <style scoped>
-    .menu {
-        grid-area: menu;
-        background: linear-gradient(to right, #232526, #414345);
+	.menu {
+		grid-area: menu;
+		background: linear-gradient(to right, #232526, #414345);
 
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-    }
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+	}
+
+	.menu a,
+	.menu a:hover {
+		color: #fff;
+		text-decoration: none;
+	}
+
+	.menu .tree-node.selected > .tree-content,
+	.menu .tree-node > .tree-content:hover {
+		background-color: rgba(255, 255, 255, 0.2);
+	}
+
+	.tree-arrow.has-child {
+		filter: brightness(2);
+	}
+
+	.menu .menu-filter {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 20px;
+		padding-bottom: 8px;
+		border-bottom: 1px solid #AAA;
+	}
+
+	.menu .menu-filter i {
+		color: #AAA;
+		margin-right: 10px;
+	}
+
+	.menu input {
+		color: #ccc;
+		font-size: 1.3rem;
+		border: 0;
+		outline: 0;
+		width: 100%;
+		background: transparent;
+	}
+
+	.tree-filter-empty {
+		color: #ccc;
+		margin-left: 20px;
+		font-size: 1.3rem;
+	}
 </style>
